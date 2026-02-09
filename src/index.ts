873f5454bcc43a985e15a79588d01de3cc6d7239
Dmytro Romanov <casteldazur@gmail.com>
2026-02-09 12:16:52 +0100

    Add main index.ts entry point

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// Main entry point for DARY React Components Library
+
+// Export all components
+export { Button } from './components/Button/Button';
+// export { BalanceCard } from './components/BalanceCard/BalanceCard';
+// export { GiftCard } from './components/GiftCard/GiftCard';
+// export { Modal } from './components/Modal/Modal';
+// export { Input } from './components/Input/Input';
+
+// Export types
+export type { ButtonProps } from './components/Button/Button';
+// Add more component types as they are created
+
+// Version
+export const VERSION = '1.0.0';
